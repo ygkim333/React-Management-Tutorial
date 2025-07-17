109ba60c9c2cc53c0843c8e673d7ad2524da96ad
ygkim333 <kikils333@hanmail.net>
2025-07-17 16:44:19 +0900

    material

--- src/App.js
+++ src/App.js
@@ -1,53 +1,84 @@
-import logo from './logo.svg';
-import Customer from './components/Customer';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Customer from "./components/Customer";
+import {
+  Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
+import { styled } from "@mui/system";
+
+// Styled 컴포넌트 정의
+const StyledPaper = styled(Paper)({
+  width: "100%",
+  marginTop: "24px",
+  overflow: "auto",
+});
+
+const StyledTable = styled(Table)({
+  minWidth: 1080,
+});
 
 const customers = [
   {
-    'id':11,
-    'image':'https://placeimg.dev/64x64/FF5733',
-    'name':'홍길동',  
-    'birthday':'12345',
-    'gender':'남자',
-    'job':'대학생'
+    id: 1,
+    image: "https://i.pravatar.cc/150?img=3",
+    name: "박창영",
+    birthday: "980630",
+    gender: "male",
+    job: "student",
   },
   {
-    'id':2,
-    'image':'https://placeimg.dev/64x64/FF5733',
-    'name':'홍길순',  
-    'birthday':'12345',
-    'gender':'남자',
-    'job':'대학생'
+    id: 2,
+    image: "https://i.pravatar.cc/150?img=1",
+    name: "홍길동",
+    birthday: "901023",
+    gender: "male",
+    job: "cooker",
   },
   {
-    'id':3,
-    'image':'https://placeimg.dev/64x64/FF5733',
-    'name':'홍길자',  
-    'birthday':'12345',
-    'gender':'남자',
-    'job':'대학생'
-  }
-]
+    id: 3,
+    image: "https://i.pravatar.cc/150?img=10",
+    name: "김수현",
+    birthday: "950101",
+    gender: "male",
+    job: "actor",
+  },
+];
 
 function App() {
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer 
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-            /> 
-          );
-        })      
-      }
-    </div>
-  ); 
+    <StyledPaper>
+      <StyledTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => (
+            <Customer
+              key={customer.id} // map 을 사용하려면 key 라는 속성이 있어야 함(안하면 Console창에 에러가 발생)
+              id={customer.id}
+              image={customer.image}
+              name={customer.name}
+              birthday={customer.birthday}
+              gender={customer.gender}
+              job={customer.job}
+            />
+          ))}
+        </TableBody>
+      </StyledTable>
+    </StyledPaper>
+  );
 }
 
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,19 @@
-import React from 'react';
+import React from "react";
+import { TableRow, TableCell } from "@mui/material";
 
-class Customer extends React.Component {
-    render(){
-        return (
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
-        )
-    }
+function Customer(props) {
+  return (
+    <TableRow>
+      <TableCell>{props.id}</TableCell>
+      <TableCell>
+        <img src={props.image} alt="profile" />
+      </TableCell>
+      <TableCell>{props.name}</TableCell>
+      <TableCell>{props.birthday}</TableCell>
+      <TableCell>{props.gender}</TableCell>
+      <TableCell>{props.job}</TableCell>
+    </TableRow>
+  );
 }
 
-class CustomerProfile extends React.Component {
-    render(){
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render(){
-        return (
-            <div>                
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 export default Customer;
