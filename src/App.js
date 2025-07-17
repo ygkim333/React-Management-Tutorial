689a9eb69fe72664ce041a5528c7f02f764b7821
ygkim333 <kikils333@hanmail.net>
2025-07-17 15:46:58 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+    'id':11,
+    'image':'https://placeimg.dev/64x64/FF5733',
+    'name':'홍길동',  
+    'birthday':'12345',
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.dev/64x64/FF5733',
+    'name':'홍길순',  
+    'birthday':'12345',
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.dev/64x64/FF5733',
+    'name':'홍길자',  
+    'birthday':'12345',
+    'gender':'남자',
+    'job':'대학생'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="log" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+            /> 
+          );
+        })      
+      }
     </div>
   ); 
 }
